4d17bb48437547fea857a8de6ad1f36a0b2196b9
minutca <svems2017@gmail.com>
2026-04-07 23:04:41 +0300

    Add files via upload

---
+++ лаб 19/лаб 19/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Таблиця множення (шістнадцяткова система):\n");
+
+        for (int i = 1; i <= 15; i++)
+        {
+            for (int j = 1; j <= 15; j++)
+            {
+                int result = i * j;
+                string hex = result.ToString("X");
+
+                Console.Write($"{hex}\t");
+            }
+            Console.WriteLine();
+        }
+    }
+}
