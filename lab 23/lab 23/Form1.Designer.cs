4d417b8d6cf25b7c4540fe3b9b49f1ec4a627a97
minutca <svems2017@gmail.com>
2026-05-24 17:47:55 +0300

    Add files via upload

---
+++ lab 23/lab 23/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace lab_23
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtStart = new System.Windows.Forms.TextBox();
+            this.txtEnd = new System.Windows.Forms.TextBox();
+            this.txtStep = new System.Windows.Forms.TextBox();
+            this.btnDraw = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictBox1
+            // 
+            this.pictBox1.Location = new System.Drawing.Point(336, 105);
+            this.pictBox1.Name = "pictBox1";
+            this.pictBox1.Size = new System.Drawing.Size(434, 318);
+            this.pictBox1.TabIndex = 0;
+            this.pictBox1.TabStop = false;
+            this.pictBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictBox1_Paint);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(520, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 26);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "x = t cos(t)\n  y = t sin(t)";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(27, 26);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Початкове t:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(27, 57);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Кінцеве t:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(27, 86);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Крок:";
+            // 
+            // txtStart
+            // 
+            this.txtStart.Location = new System.Drawing.Point(103, 19);
+            this.txtStart.Name = "txtStart";
+            this.txtStart.Size = new System.Drawing.Size(67, 20);
+            this.txtStart.TabIndex = 5;
+            // 
+            // txtEnd
+            // 
+            this.txtEnd.Location = new System.Drawing.Point(103, 54);
+            this.txtEnd.Name = "txtEnd";
+            this.txtEnd.Size = new System.Drawing.Size(67, 20);
+            this.txtEnd.TabIndex = 6;
+            // 
+            // txtStep
+            // 
+            this.txtStep.Location = new System.Drawing.Point(103, 86);
+            this.txtStep.Name = "txtStep";
+            this.txtStep.Size = new System.Drawing.Size(67, 20);
+            this.txtStep.TabIndex = 7;
+            // 
+            // btnDraw
+            // 
+            this.btnDraw.Location = new System.Drawing.Point(30, 140);
+            this.btnDraw.Name = "btnDraw";
+            this.btnDraw.Size = new System.Drawing.Size(98, 34);
+            this.btnDraw.TabIndex = 8;
+            this.btnDraw.Text = "Побудувати";
+            this.btnDraw.UseVisualStyleBackColor = true;
+            this.btnDraw.Click += new System.EventHandler(this.btnDraw_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnDraw);
+            this.Controls.Add(this.txtStep);
+            this.Controls.Add(this.txtEnd);
+            this.Controls.Add(this.txtStart);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictBox1);
+            this.Name = "Form1";
+            this.Text = "lblFormula";
+         
+            ((System.ComponentModel.ISupportInitialize)(this.pictBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtStart;
+        private System.Windows.Forms.TextBox txtEnd;
+        private System.Windows.Forms.TextBox txtStep;
+        private System.Windows.Forms.Button btnDraw;
+    }
+}
+
---
+++ lab 23/lab 23/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+
+namespace lab_23
+{
+    public partial class Form1 : Form
+    {
+        double tStart = 0;
+        double tEnd = 10 * Math.PI;
+        double tStep = 0.01;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictBox1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+
+            g.SmoothingMode = SmoothingMode.AntiAlias;
+            g.Clear(Color.White);
+
+            int w = pictBox1.Width;
+            int h = pictBox1.Height;
+
+            int centerX = w / 2;
+            int centerY = h / 2;
+
+            List<PointF> points = new List<PointF>();
+
+            double scale = 10;
+
+            for (double t = tStart;
+                 t <= tEnd;
+                 t += tStep)
+            {
+                double xVal = t * Math.Cos(t);
+                double yVal = t * Math.Sin(t);
+
+                float screenX =
+                    (float)(centerX + xVal * scale);
+
+                float screenY =
+                    (float)(centerY - yVal * scale);
+
+                points.Add(
+                    new PointF(
+                        screenX,
+                        screenY));
+            }
+
+            if (points.Count > 1)
+            {
+                g.DrawLines(
+                    new Pen(Color.Red, 2),
+                    points.ToArray());
+            }
+        }
+
+        private void btnDraw_Click(
+            object sender,
+            EventArgs e)
+        {
+            tStart =
+                Convert.ToDouble(
+                    txtStart.Text);
+
+            tEnd =
+                Convert.ToDouble(
+                    txtEnd.Text);
+
+            tStep =
+                Convert.ToDouble(
+                    txtStep.Text);
+
+            pictBox1.Invalidate();
+        }
+    }
+}
