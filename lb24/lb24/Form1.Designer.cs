4094cdda03309c236d95021ff10b0b73eae7c8b9
minutca <svems2017@gmail.com>
2026-06-01 12:05:12 +0300

    Add files via upload

---
+++ lb24/lb24/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace lb24
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(46, 53);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(103, 20);
+            this.txtInput.TabIndex = 0;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(320, 44);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(468, 394);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(46, 152);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "Старт";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(185, 152);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 3;
+            this.btnStop.Text = "Стоп";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.txtInput);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+    }
+}
+
---
+++ lb24/lb24/Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace lb24
+{
+    public partial class Form1 : Form
+    {
+        Thread thread1;
+        Thread thread2;
+        Thread thread3;
+
+        bool work = true;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            thread1 = new Thread(Method1);
+            thread2 = new Thread(Method2);
+            thread3 = new Thread(Method3);
+        }
+
+        private void Method1()
+        {
+            while (work)
+            {
+                try
+                {
+                    string text = txtInput.Text;
+
+                    byte[] data = Encoding.UTF8.GetBytes(text);
+
+                    using (Aes aes = Aes.Create())
+                    {
+                        ICryptoTransform encryptor = aes.CreateEncryptor();
+
+                        byte[] encrypted =
+                            encryptor.TransformFinalBlock(data, 0, data.Length);
+
+                        Invoke((MethodInvoker)delegate
+                        {
+                            richTextBox1.AppendText(
+                                "CA-1.1: " +
+                                Convert.ToBase64String(encrypted) +
+                                Environment.NewLine);
+                        });
+                    }
+                }
+                catch { }
+
+                Thread.Sleep(2000);
+            }
+        }
+
+        private void Method2()
+        {
+            while (work)
+            {
+                try
+                {
+                    string text = txtInput.Text;
+
+                    SHA256 sha = SHA256.Create();
+
+                    byte[] hash =
+                        sha.ComputeHash(Encoding.UTF8.GetBytes(text));
+
+                    Invoke((MethodInvoker)delegate
+                    {
+                        richTextBox1.AppendText(
+                            "Хеш: " +
+                            BitConverter.ToString(hash) +
+                            Environment.NewLine);
+                    });
+                }
+                catch { }
+
+                Thread.Sleep(2000);
+            }
+        }
+
+        private void Method3()
+        {
+            Random rnd = new Random();
+
+            while (work)
+            {
+                try
+                {
+                    int number = rnd.Next(1, 100000);
+
+                    Invoke((MethodInvoker)delegate
+                    {
+                        richTextBox1.AppendText(
+                            "Nanoteq: " +
+                            number +
+                            Environment.NewLine);
+                    });
+                }
+                catch { }
+
+                Thread.Sleep(1000);
+            }
+        }
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            work = true;
+
+            thread1 = new Thread(Method1);
+            thread2 = new Thread(Method2);
+            thread3 = new Thread(Method3);
+
+            thread1.Start();
+            thread2.Start();
+            thread3.Start();
+        }
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            work = false;
+        }
+            private void btnStop_Click(object sender, EventArgs e)
+        {
+            work = false;
+
+            if (thread1 != null && thread1.IsAlive)
+                thread1.Abort();
+
+            if (thread2 != null && thread2.IsAlive)
+                thread2.Abort();
+
+            if (thread3 != null && thread3.IsAlive)
+                thread3.Abort();
+        }
+    }
+    }
