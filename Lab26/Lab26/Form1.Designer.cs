e4b95caa6319c1bc317c9998567cb32b7b37eae2
minutca <svems2017@gmail.com>
2026-06-04 16:02:14 +0300

    Add files via upload

---
+++ Lab26/Lab26/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace Lab26
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtReason = new System.Windows.Forms.TextBox();
+            this.txtDate = new System.Windows.Forms.TextBox();
+            this.btnCreate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(25, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "ПІБ";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(111, 30);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 20);
+            this.txtName.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(21, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(113, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "За що народжується";
+       
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(21, 99);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(33, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Дата";
+            // 
+            // txtReason
+            // 
+            this.txtReason.Location = new System.Drawing.Point(140, 64);
+            this.txtReason.Name = "txtReason";
+            this.txtReason.Size = new System.Drawing.Size(100, 20);
+            this.txtReason.TabIndex = 7;
+            // 
+            // txtDate
+            // 
+            this.txtDate.Location = new System.Drawing.Point(111, 99);
+            this.txtDate.Name = "txtDate";
+            this.txtDate.Size = new System.Drawing.Size(100, 20);
+            this.txtDate.TabIndex = 9;
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.Location = new System.Drawing.Point(24, 146);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(181, 23);
+            this.btnCreate.TabIndex = 11;
+            this.btnCreate.Text = "Створити грамоту";
+            this.btnCreate.UseVisualStyleBackColor = true;
+
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnCreate);
+            this.Controls.Add(this.txtDate);
+            this.Controls.Add(this.txtReason);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtReason;
+        private System.Windows.Forms.TextBox txtDate;
+        private System.Windows.Forms.Button btnCreate;
+    }
+}
+
---
+++ Lab26/Lab26/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Windows.Forms;
+using Word = Microsoft.Office.Interop.Word;
+
+namespace Lab26
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCreate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string templatePath = @"C:\Lab26\gramota.docx";
+
+                Word.Application wordApp = new Word.Application();
+
+                Word.Document document =
+                    wordApp.Documents.Add(templatePath);
+
+                FindAndReplace(wordApp, "<<NAME>>", txtName.Text);
+                FindAndReplace(wordApp, "<<REASON>>", txtReason.Text);
+                FindAndReplace(wordApp, "<<DATE>>", txtDate.Text);
+
+                wordApp.Visible = true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(
+                    "Помилка: " + ex.Message,
+                    "Помилка",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error
+                );
+            }
+        }
+
+        private void FindAndReplace(
+            Word.Application wordApp,
+            object findText,
+            object replaceWithText)
+        {
+            object matchCase = true;
+            object matchWholeWord = true;
+            object matchWildCards = false;
+            object matchSoundsLike = false;
+            object matchAllWordForms = false;
+            object forward = true;
+            object format = false;
+            object replace = 2;
+            object wrap = 1;
+
+            wordApp.Selection.Find.Execute(
+                ref findText,
+                ref matchCase,
+                ref matchWholeWord,
+                ref matchWildCards,
+                ref matchSoundsLike,
+                ref matchAllWordForms,
+                ref forward,
+                ref wrap,
+                ref format,
+                ref replaceWithText,
+                ref replace
+            );
+        }
+    }
+}
